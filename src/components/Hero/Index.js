3663933d9e7ac5d7c0d9474e30a155b60649af20
viadel13 <shababizetre@gmail.com>
2023-04-30 16:00:56 +0100

    first commit

--- src/components/Hero/Index.js
+++ src/components/Hero/Index.js
@@ -1,89 +1,47 @@
-import { useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 import hero2 from "../../assets/images/hero2.jpg";
 import hero3 from "../../assets/images/hero3.jpg";
 import YouPain from "../../assets/images/you.png";
 
 const Hero = () => {
-  const [slides, setSlides] = useState([]);
 
-  useEffect(() => {
-    // Récupérer le tableau des slides à partir du localStorage s'il existe
-    const slidesFromLocalStorage = localStorage.getItem("slides");
-    if (slidesFromLocalStorage) {
-      setSlides(JSON.parse(slidesFromLocalStorage));
-    } else {
-      // Ajouter les images en slide à un tableau et le stocker dans le localStorage
-      const images = [hero3, hero2, YouPain];
-      const slides = images.map((image) => ({
-        image,
-        caption: "Some representative placeholder content",
-      }));
-      localStorage.setItem("slides", JSON.stringify(slides));
-      setSlides(slides);
-    }
-  }, []);
+  SwiperCore.use([Navigation, Pagination, Autoplay]);
 
   return (
     <>
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide"
-        data-bs-ride="carousel"
+     <Swiper
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        className="mySwiper"
+        autoplay={{delay: 4000}}
       >
-        <div className="carousel-indicators">
-          {slides.map((slide, index) => (
-            <button
-              key={index}
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to={index}
-              className={index === 0 ? "active" : ""}
-              aria-current={index === 0 ? "true" : "false"}
-              aria-label={`Slide ${index + 1}`}
-            ></button>
-          ))}
-        </div>
-        <div className="carousel-inner">
-          {slides.map((slide, index) => (
-            <div
-              key={index}
-              className={`carousel-item ${index === 0 ? "active" : ""}`}
-              id="slide-Your"
-            >
-              <img
-                src={slide.image}
-                className= {`d-block w-100 ${index === 0 || index === 1 ? 'hero-slider' : 'Yourpain'}`}
-                alt="hero"
-              />
-              <div className="carousel-caption">
-                <h5>{`Slide ${index + 1} label`}</h5>
-                <p>{slide.caption}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+        <SwiperSlide>
+          <img src={hero3} className="d-block  hero-slider" alt="hero" />
+          <div className="carousel-caption">
+            {/* <img src={YouPain} className="img-fluid Yourpain" alt="Yourpain" /> */}
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hero2} className="d-block w-100 hero-slider" alt="hero" />
+          <div className="carousel-caption carousel-customer">
+            {/* <img src={YouPain} className="img-fluid Yourpain" alt="Yourpain" /> */}
+            <h5 className="mt-4">Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="justify-content-center" id="slide-Your">
+          <img src={YouPain} className="d-block Yourpain" alt="Yourpain" />
+        </SwiperSlide>
+      </Swiper>
+   
     </>
   );
 };
